452a90135201ed1e9845de7514ef9a67acb91003
100ngram <sangramjitdutta@gmail.com>
2024-07-18 16:45:50 +0530

    feat: loader button in create clip form

--- app/_components/create-clip-form.tsx
+++ app/_components/create-clip-form.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useAuth } from "@clerk/nextjs";
 import { useToast } from "@/components/ui/use-toast";
 import { useRouter } from "next/navigation";
 import axiosInstance from "@/lib/axiosInstance";
+import { LoaderButton } from "@/components/ui/loader-button";
 
 const schema = z.object({
   title: z
@@ -24,6 +24,8 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>;
 
 export const CreateClipForm: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const { toast } = useToast();
   const router = useRouter();
   const { userId } = useAuth();
@@ -38,6 +40,7 @@ export const CreateClipForm: React.FC = () => {
 
   const onSubmit: SubmitHandler<FormData> = async (data) => {
     try {
+      setIsLoading(true);
       console.log("host", process.env.HOST);
       let bodyContent = JSON.stringify({
         title: data.title,
@@ -55,12 +58,15 @@ export const CreateClipForm: React.FC = () => {
       toast({
         title: "Clip Published !",
       });
+      setIsLoading(false);
+
       router.push("/dashboard");
     } catch (e: any) {
       console.log("error", e);
       toast({
         title: JSON.stringify(e),
       });
+      setIsLoading(false);
     }
   };
 
@@ -87,9 +93,9 @@ export const CreateClipForm: React.FC = () => {
           <p className="text-red-500 text-sm mt-1">{errors.content.message}</p>
         )}
       </div>
-      <Button type="submit" className="w-full">
+      <LoaderButton type="submit" isLoading={isLoading}>
         Publish Clip
-      </Button>
+      </LoaderButton>
     </form>
   );
 };
