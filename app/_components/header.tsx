cddcb2a7b02dab60ec051beada843b736db3ba26
100ngram <sangramjitdutta@gmail.com>
2024-07-20 15:57:03 +0530

    feat: search button in mobile screens

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { UserButton, SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
@@ -15,20 +15,18 @@ import {
 import { FileCode2, PanelRightOpen, Search } from "lucide-react";
 import { ModeToggle } from "./theme-toggle";
 import {
-  Command,
   CommandDialog,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
   CommandList,
-  CommandShortcut,
 } from "@/components/ui/command";
 
 function Header() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const down = (e: {
       key: string;
       metaKey: any;
@@ -46,38 +44,57 @@ function Header() {
 
   return (
     <header>
-      <nav className="flex justify-between items-center p-6">
-        <div className="flex justify-start gap-2">
-          <FileCode2 className="w-6 h-6" />
-          <span>codeclip</span>
+      <nav className="flex justify-between items-center p-3 sm:p-6">
+        <div className="flex justify-start gap-1 sm:gap-2">
+          <FileCode2 className="w-4 h-4 sm:w-6 sm:h-6" />
+          <span className="text-sm sm:text-base">codeclip</span>
         </div>
-        <div className="hidden md:flex space-x-6 items-center">
-          <Link href="/dashboard">Home</Link>
-          <Link href="/dashboard/explore">Explore</Link>
-          <Link href="/dashboard/create">Create</Link>
+        <div className="hidden md:flex space-x-3 sm:space-x-6 items-center">
+          <Link href="/dashboard" className="text-sm sm:text-base">
+            Home
+          </Link>
+          <Link href="/dashboard/explore" className="text-sm sm:text-base">
+            Explore
+          </Link>
+          <Link href="/dashboard/create" className="text-sm sm:text-base">
+            Create
+          </Link>
           <Button
             variant="outline"
-            className="w-[200px] justify-between"
+            className="w-[150px] sm:w-[200px] justify-between text-xs sm:text-sm"
             onClick={() => setOpen(true)}
           >
-            <Search className="mr-2 h-4 w-4" />
+            <Search className="mr-1 sm:mr-2 h-3 w-3 sm:h-4 sm:w-4" />
             Search...
-            <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-              <span className="text-xs">⌘</span>K
+            <kbd className="hidden sm:inline-flex pointer-events-none h-4 sm:h-5 select-none items-center gap-1 rounded border bg-muted px-1 sm:px-1.5 font-mono text-[8px] sm:text-[10px] font-medium text-muted-foreground opacity-100">
+              <span className="text-[8px] sm:text-xs">⌘</span>K
             </kbd>
           </Button>
         </div>
-
-        <div className="md:hidden flex gap-2">
+        <div className="md:hidden flex gap-1 sm:gap-2">
+          <Button
+            variant="outline"
+            className="w-[100px] sm:w-[150px] justify-between text-xs sm:text-sm"
+            onClick={() => setOpen(true)}
+          >
+            <Search className="mr-1 sm:mr-2 h-3 w-3 sm:h-4 sm:w-4" />
+            Search...
+          </Button>
+        </div>
+        <div className="md:hidden flex gap-1 sm:gap-2">
           <div>
             <SignedOut>
-              <SignInButton />
+              <SignInButton>
+                <Button size="sm" className="text-xs sm:text-sm">
+                  Sign In
+                </Button>
+              </SignInButton>
             </SignedOut>
             <SignedIn>
               <UserButton
                 appearance={{
                   elements: {
-                    userButtonAvatarBox: "w-9 h-9",
+                    userButtonAvatarBox: "w-6 h-6 sm:w-9 sm:h-9",
                   },
                 }}
               />
@@ -85,8 +102,12 @@ function Header() {
           </div>
           <Sheet>
             <SheetTrigger asChild>
-              <Button variant="outline" size="icon">
-                <PanelRightOpen className="w-4 h-4" />
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-7 w-7 sm:h-9 sm:w-9"
+              >
+                <PanelRightOpen className="w-3 h-3 sm:w-4 sm:h-4" />
               </Button>
             </SheetTrigger>
             <SheetContent>
@@ -94,25 +115,37 @@ function Header() {
                 <SheetTitle></SheetTitle>
                 <SheetDescription></SheetDescription>
               </SheetHeader>
-              <div className="grid gap-6 p-2">
+              <div className="grid gap-3 sm:gap-6 p-2">
                 <SheetClose asChild>
-                  <Link href="/dashboard">Home</Link>
+                  <Link href="/dashboard" className="text-sm sm:text-base">
+                    Home
+                  </Link>
                 </SheetClose>
                 <SheetClose asChild>
-                  <Link href="/dashboard/explore">Explore</Link>
+                  <Link
+                    href="/dashboard/explore"
+                    className="text-sm sm:text-base"
+                  >
+                    Explore
+                  </Link>
                 </SheetClose>
                 <SheetClose asChild>
-                  <Link href="/dashboard/create">Create</Link>
+                  <Link
+                    href="/dashboard/create"
+                    className="text-sm sm:text-base"
+                  >
+                    Create
+                  </Link>
                 </SheetClose>
                 <Button
                   variant="outline"
-                  className="w-[200px] justify-between"
+                  className="w-[150px] sm:w-[200px] justify-between text-xs sm:text-sm"
                   onClick={() => setOpen(true)}
                 >
-                  <Search className="mr-2 h-4 w-4" />
+                  <Search className="mr-1 sm:mr-2 h-3 w-3 sm:h-4 sm:w-4" />
                   Search...
-                  <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-                    <span className="text-xs">⌘</span>K
+                  <kbd className="pointer-events-none inline-flex h-4 sm:h-5 select-none items-center gap-1 rounded border bg-muted px-1 sm:px-1.5 font-mono text-[8px] sm:text-[10px] font-medium text-muted-foreground opacity-100">
+                    <span className="text-[8px] sm:text-xs">⌘</span>K
                   </kbd>
                 </Button>
                 <ModeToggle />
@@ -121,16 +154,20 @@ function Header() {
           </Sheet>
         </div>
 
-        <div className="hidden md:flex space-x-4">
+        <div className="hidden md:flex space-x-2 sm:space-x-4">
           <ModeToggle />
           <SignedOut>
-            <SignInButton />
+            <SignInButton>
+              <Button size="sm" className="text-xs sm:text-sm">
+                Sign In
+              </Button>
+            </SignInButton>
           </SignedOut>
           <SignedIn>
             <UserButton
               appearance={{
                 elements: {
-                  userButtonAvatarBox: "w-7 h-7",
+                  userButtonAvatarBox: "w-6 h-6 sm:w-7 sm:h-7",
                 },
               }}
             />
@@ -143,8 +180,8 @@ function Header() {
           <CommandEmpty>No results found.</CommandEmpty>
           <CommandGroup heading="Suggestions">
             <CommandItem>
-              <Search className="mr-2 h-4 w-4" />
-              <span>Search</span>
+              <Search className="mr-2 h-3 w-3 sm:h-4 sm:w-4" />
+              <span className="text-sm sm:text-base">Search</span>
             </CommandItem>
           </CommandGroup>
         </CommandList>
