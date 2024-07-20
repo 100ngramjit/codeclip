57cde674de947fd758576c9b795a272c0be21f97
100ngram <sangramjitdutta@gmail.com>
2024-07-20 17:29:05 +0530

    feat: search API integration

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Button } from "@/components/ui/button";
 import { UserButton, SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
@@ -22,9 +22,12 @@ import {
   CommandItem,
   CommandList,
 } from "@/components/ui/command";
+import debounce from "lodash/debounce";
 
 function Header() {
   const [open, setOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     const down = (e: {
@@ -41,6 +44,32 @@ function Header() {
     document.addEventListener("keydown", down);
     return () => document.removeEventListener("keydown", down);
   }, []);
+
+  const debouncedSearch = useCallback(
+    debounce(async (query: string) => {
+      if (query) {
+        const res = await fetch("/api/search", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ searchTerm: query }),
+        });
+        const data = await res.json();
+        data.map((ele: any) => console.log(ele));
+        setSearchResults(data || []);
+      } else {
+        setSearchResults([]);
+      }
+    }, 300),
+    []
+  );
+
+  const handleSearchChange = (e: any) => {
+    const query = e.target.value;
+    setSearchTerm(query);
+    debouncedSearch(query);
+  };
 
   return (
     <header>
@@ -175,15 +204,22 @@ function Header() {
         </div>
       </nav>
       <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="Type a command or search..." />
+        <CommandInput
+          placeholder="Type a command or search..."
+          value={searchTerm}
+          onChangeCapture={handleSearchChange}
+        />
         <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
-            <CommandItem>
-              <Search className="mr-2 h-3 w-3 sm:h-4 sm:w-4" />
-              <span className="text-sm sm:text-base">Search</span>
-            </CommandItem>
-          </CommandGroup>
+          {!searchResults.length ? (
+            <CommandEmpty>No results found.</CommandEmpty>
+          ) : (
+            <div>
+              <h4 className="text-muted-foreground">Search Results</h4>
+              {searchResults.map((result: any, index) => (
+                <p key={index}>{result.fileName}</p>
+              ))}
+            </div>
+          )}
         </CommandList>
       </CommandDialog>
     </header>
