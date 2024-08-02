d058423b1942ea185ea1b16ca50729c4ad24d286
100ngram <sangramjitdutta@gmail.com>
2024-08-02 10:37:41 +0530

    feat: check icon after clicking copy button

--- app/_components/copy-button.tsx
+++ app/_components/copy-button.tsx
@@ -1,22 +1,29 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
-import { Copy } from "lucide-react";
+import { Copy, Check } from "lucide-react";
 
-export function CopyButton({ text }: any) {
+export function CopyButton({ text }: { text: string }) {
   const { toast } = useToast();
+  const [showTick, setShowTick] = useState(false);
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(text);
+    toast({ title: "Copied to clipboard!" });
+    setShowTick(true);
+    setTimeout(() => setShowTick(false), 3000);
+  };
+
   return (
     <Button
       size="icon"
       variant="ghost"
       className="h-6 w-6 cursor-pointer"
-      onClick={() => {
-        navigator.clipboard.writeText(text);
-        toast({ title: "copied to clipboard !" });
-      }}
+      onClick={handleClick}
     >
-      <Copy className="w-4 h-4" />
+      {showTick ? <Check className="w-4 h-4" /> : <Copy className="w-4 h-4" />}
     </Button>
   );
 }
