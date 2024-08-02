ef6457383cde40a962eb0e8c4d0eecec05a21e41
100ngram <sangramjitdutta@gmail.com>
2024-08-02 13:53:49 +0530

    feat: saved clips api routes

---
+++ app/api/saved/route.ts
@@ -0,0 +1,24 @@
+import { deleteSavedClip, getSavedClips, saveClip } from "@/db/queries";
+
+export async function POST(req: Request) {
+  const { clipId, userId } = await req.json();
+  const response = await saveClip(clipId, userId);
+
+  return Response.json(response);
+}
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const userId = url.searchParams.get("userid");
+  const response = await getSavedClips(userId!);
+  console.log(response);
+
+  return Response.json(response);
+}
+
+export async function DELETE(req: Request) {
+  const { userId, savedClipId } = await req.json();
+  const response = await deleteSavedClip(savedClipId, userId);
+
+  return Response.json(response);
+}
